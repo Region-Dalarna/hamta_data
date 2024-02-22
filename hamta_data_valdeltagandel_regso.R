9d6e9292338254da7926a39b908e0fd7554210fa
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2024-02-22 09:07:43 +0100

    Uppdaterat skript till samma upplägg som ohälsotal

--- hamta_data_valdeltagandel_regso.R
+++ hamta_data_valdeltagandel_regso.R
@@ -1,30 +1,36 @@
 hamta_data_valdeltagande_regso <- function(region_vekt = "20",
-                                           Cont_code = "000004WU", # För val, se nedan
-                                           bakgrund_klartext = "samtliga", # För alternativ, se nedan
-                                           kon_klartext = "män och kvinnor", # c("män","kvinnor") ger uppdelat. "män och kvinnor" ger totalt
-                                           output_mapp = NA, # Outputmapp. Sätts till en mapp om data skall sparas
-                                           filnamn = "valdeltagande_regso.xlsx", # Filnamn. Ändra om man vill köra ut en för regso respektive deso
-                                           returnera_data = TRUE, # Om man vill returnera data
-                                           tid = "*") # Sätts till "9999" om man enbart vill ha senaste år, alternativt ett intervall som slutar på "9999". "*" ger samtliga år
+                                     cont_klartext = "Valdeltagande i val till kommun, procent", # Finns: "Valdeltagande i val till riksdag, procent", "Valdeltagande i val till region, procent"
+                                     bakgrund_klartext = c("samtliga"), # Finns: "samtliga 20-64 år", "ålder: 20-24 år", "ålder: 25-34 år", "ålder: 35-44 år", 
+                                     #         "ålder: 45-54 år", "ålder: 55-64 år", "samtliga utbildningsnivåer", 
+                                     #         "utbildningsnivå: förgymnasial utbildning", "utbildningsnivå: gymnasial utbildning", 
+                                     #         "utbildningsnivå: eftergymnasial utbildning", "utbildningsnivå: uppgift saknas", 
+                                     #         "samtliga", "födelseregion: Sverige", "födelseregion: Norden exkl. Sverige", 
+                                     #         "födelseregion: EU/EFTA exkl. Norden", "födelseregion: övriga världen", 
+                                     #         "samtliga utrikes födda invandrare", "samtliga utrikes födda invandrare", 
+                                     #         "skäl till invandring: skyddsbehövande och deras anhöriga", 
+                                     #         "skäl till invandring: övriga utrikes födda invandrare", "samtliga utrikes födda", 
+                                     #         "vistelsetid 0-1 år", "vistelsetid 2-3 år", "vistelsetid 4-9 år", "vistelsetid 10- år"
+                                     kon_klartext = "män och kvinnor",  # Finns: "män och kvinnor", "män", "kvinnor"
+                                     output_mapp = NA,                  # Outputmapp. Sätts till en mapp om data skall sparas
+                                     filnamn = "valdeltagande_regso.xlsx",  # Filnamn om man vill spara en excelfil i output_mapp.
+                                     returnera_data = TRUE,             # Om man vill returnera data som en dataframe från funktionen
+                                     long_format = TRUE,                # om man tar med fler än en innehållsvariabel så görs format om från wide till long
+                                     tid = "*")                         # Sätts till "9999" om man enbart vill ha senaste år, alternativt ett intervall som slutar 
+# på "9999". "*" ger samtliga år
 {
   
   # ===========================================================================================================
   #
-  # Skript för att hämta data för valdeltagande. Finns enbart för Regso
+  # Skript för att hämta data för valdeltagande (på olika nivåer) kopplat till Regso. Källa:
+  # https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__AA__AA0003__AA0003J/IntGr11RegSO1/
   # 
   # För att få en djupare förklaring av vad som de olika kategorierna under varje variabel betyder, använd:
   # pxvardelist("https://api.scb.se/OV0104/v1/doris/sv/ssd/START/AA/AA0003/AA0003J/IntGr11RegSO1", "Bakgrund")
   # För att få en förståelse för alla variabler som finns, använd
   # pxvarlist("https://api.scb.se/OV0104/v1/doris/sv/ssd/START/AA/AA0003/AA0003J/IntGr11RegSO1")
-  # Kräver att source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R") har körts först
-  # Förklaringar av variabler
-  # Cont_code:
-  # 000004WU Valdeltagande i val till riksdag, procent
-  # 000004WV  Valdeltagande i val till region, procent
-  # 000004WW  Valdeltagande i val till kommun, procent
-  # där man byter mot den variabel man är intresserad av.
-  # Skapad av Jon Frank 2024-01-26
-  # 
+  # Båda kräver  att source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R") har körts först
+  #
+  # Skapad av Jon Frank 2024-02-22
   # ===========================================================================================================
   
   if (!require("pacman")) install.packages("pacman")
@@ -34,15 +40,16 @@ hamta_data_valdeltagande_regso <- function(region_vekt = "20",
   
   # "Adresser" till SCBs databas
   url_uttag <- "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/AA/AA0003/AA0003J/IntGr11RegSO1"
+  px_meta <- pxweb_get(url_uttag)          # vi hämtar metadata till tabellen här och gör inga fler uttag nedan = färre API-anrop (och elegantare lösning)
+  
+  region_indelning = "RegSO"
   
   # Data som sourcas från Region Dalarna
   source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R")
   
-  region_indelning = "RegSO"
-  
   ########### Hanterar Regso/Deso #######################################################################################
   # vi hämtar alla regionkoder i databasen
-  alla_regionkoder <- hamta_giltiga_varden_fran_tabell(url_uttag, "region") 
+  alla_regionkoder <- hamta_giltiga_varden_fran_tabell(px_meta, "region") 
   # vi tar ut endast RegSO eller DeSO beroende på vad användaren valt
   alla_regionkoder <- if(region_indelning == "RegSO") alla_regionkoder[str_detect(alla_regionkoder, "R")] else alla_regionkoder[str_detect(alla_regionkoder, "A|B|C")]
   
@@ -63,21 +70,17 @@ hamta_data_valdeltagande_regso <- function(region_vekt = "20",
   #########################################################################################################################
   
   # Gör om från klartext
-  kon_vekt <- hamta_kod_med_klartext(url_uttag, kon_klartext, skickad_fran_variabel = "kon")
+  kon_vekt <- hamta_kod_med_klartext(px_meta, kon_klartext, skickad_fran_variabel = "kon")
+  cont_vekt <- hamta_kod_med_klartext(px_meta, cont_klartext, skickad_fran_variabel = "contentscode")
+  bakgrund_vekt <- if (all(bakgrund_klartext == "*")) "*" else hamta_kod_med_klartext(px_meta, bakgrund_klartext, skickad_fran_variabel = "bakgrund")
   
-  if ( all(bakgrund_klartext == "*")){
-    
-    bakgrund_vekt = "*"
-    
-  }else bakgrund_vekt <- hamta_kod_med_klartext(url_uttag, bakgrund_klartext, skickad_fran_variabel = "bakgrund")
-  
-  giltiga_ar <- hamta_giltiga_varden_fran_tabell(url_uttag, "tid")
+  giltiga_ar <- hamta_giltiga_varden_fran_tabell(px_meta, "tid")
   if (all(tid != "*")) tid <- tid %>% as.character() %>% str_replace("9999", max(giltiga_ar)) %>% .[. %in% giltiga_ar] %>% unique()
   
   varlista <- list(Region = alla_region,
                    Bakgrund = bakgrund_vekt,
-                   Kon= kon_vekt,
-                   ContentsCode = Cont_code,
+                   Kon = kon_vekt,
+                   ContentsCode = cont_vekt,
                    Tid = tid)
   
   px_uttag <- pxweb_get(url = url_uttag,query = varlista)
@@ -88,6 +91,12 @@ hamta_data_valdeltagande_regso <- function(region_vekt = "20",
             select(regionkod = Region)) %>%
     relocate(regionkod, .before = region) 
   
+  # man kan välja bort long-format, då låter vi kolumnerna vara wide om det finns fler innehållsvariabler, annars
+  # pivoterar vi om till long-format, dock ej om det bara finns en innehållsvariabel
+  if (long_format & length(cont_klartext) > 1) {
+    valdeltagande <- valdeltagande %>% 
+      konvertera_till_long_for_contentscode_variabler(api_url = px_meta, content_var = "ohalso_variabel")
+  } # slut if-sats som kontrollera om vi vill ha df i long-format
   
   if (!is.na(output_mapp) & !is.na(filnamn)){
     write.xlsx(valdeltagande,paste0(output_mapp,filnamn))
