b1cd134beb7fccd9f9df3128a8c6f18c82ed9485
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2024-03-05 13:12:03 +0100

    Nytt skript (arbetsmarknadsstatus slutgiltig data)

--- hamta_data_arbetsmarknadsstatus_bas_ar_slutgiltig.R
+++ hamta_data_arbetsmarknadsstatus_bas_ar_slutgiltig.R
@@ -1,10 +1,10 @@
-hamta_arbetsmarknadsstatus_bas_ar_prel <- function(
+hamta_arbetsmarknadsstatus_bas_ar_slutgiltig <- function(
     region_vekt = "20",			# Val av region.
     kon_klartext = "*",			 #  NA = tas inte med i uttaget,  Finns: "män", "kvinnor", "totalt"
     alder_klartext = "*",			 #  Finns: "15-19 år", "16-19 år", "20-24 år", "25-29 år", "30-34 år", "35-39 år", "40-44 år", "45-49 år", "50-54 år", "55-59 år", "60-64 år", "65-69 år", "70-74 år", "15-74 år", "16-64 år", "16-65 år", "20-64 år", "20-65 år"
     fodelseregion_klartext = "*",			#  NA = tas inte med i uttaget,  Finns: "inrikes född", "utrikes född", "totalt"
     cont_klartext = "*",			 #  Finns: "antal sysselsatta", "antal arbetslösa", "antal sysselsatta och arbetslösa (arbetskraften)", "antal studerande", "antal pensionärer", "antal sjuka", "antal övriga", "antal totalt", "arbetslöshet", "arbetskraftsdeltagande", "sysselsättningsgrad"
-    tid_koder = "*",			 # "*" = alla år eller månader, "9999" = senaste, finns: "2020", "2021", "2022", "2023"
+    tid_koder = "*",			 # "*" = alla år eller månader, "9999" = senaste, finns: "2020", "2021", "2022"
     long_format = TRUE,			# TRUE = konvertera innehållsvariablerna i datasetet till long-format 
     wide_om_en_contvar = TRUE,			# TRUE = om man vill behålla wide-format om det bara finns en innehållsvariabel, FALSE om man vill konvertera till long-format även om det bara finns en innehållsvariabel
     output_mapp = NA,			# anges om man vill exportera en excelfil med uttaget, den mapp man vill spara excelfilen till
@@ -14,16 +14,14 @@ hamta_arbetsmarknadsstatus_bas_ar_prel <- function(
   
   # ====================================================================================================
   #
-  # Funktion för att hämta data från SCB:s API med hjälp av pxweb-paketet. Innehåller preliminär data för 
-  # arbetsmarknadsstatus baserat på ålder, kön, födelseregion, region och år.
-  # Notera att preliminär data kan vara ofullständig och ändras i senare versioner. 
-  # Använd därför även skriptet hamta_data_arbetsmarknadsstatus_bas_ar_slutgiltig.R för att hämta slutgiltig data där sådan finns tillgänglig.
+  # Funktion för att hämta data från SCB:s API med hjälp av pxweb-paketet. 
+  # Innehåller slutgiltig data för arbetsmarknadsstatus baserat på ålder, kön, födelseregion, region och år.
   # Automatgenererat av en funktion i R som skrivits av Peter Möller, Region Dalarna
   #
   # Skapad av: frkjon den 05 mars 2024
   # Senast uppdaterad: 05 mars 2024
   #
-  # url till tabellens API: https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__AM__AM0210__AM0210A/ArbStatusAr/
+  # url till tabellens API: https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__AM__AM0210__AM0210D/ArRegArbStatus/
   #
   # ====================================================================================================
   
@@ -36,7 +34,7 @@ hamta_arbetsmarknadsstatus_bas_ar_prel <- function(
   source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R")
   
   # Url till SCB:s databas
-  url_uttag <- "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/AM/AM0210/AM0210A/ArbStatusAr"
+  url_uttag <- "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/AM/AM0210/AM0210D/ArRegArbStatus"
   px_meta <- pxweb_get(url_uttag)
   
   varlist_koder <- pxvarlist(px_meta)$koder
