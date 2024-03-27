85e663450b795e4953bb90da59c9cf65f7dcee4a
FaluPeppe <72673226+FaluPeppe@users.noreply.github.com>
2024-03-27 14:46:01 +0100

    Ny version av helårsekvivalentskriptet, med möjlighet att hämta län och att aggregera till större geografiska enheter.

---
+++ hamta_helarsekvivalenter_region_kon_aldersgrupp_tid_HE0000T02N2_scb.R
@@ -0,0 +1,88 @@
+hamta_helarsekvivalenter_region_kon_aldersgrupp_tid_scb <- function(
+			region_vekt = "00",			# Val av region.
+			kon_klartext = "*",			 #  NA = tas inte med i uttaget,  Finns: "män", "kvinnor", "män och kvinnor totalt"
+			aldersgrupp_klartext = "*",			 #  Finns: "20-64 år", "20-65 år"
+			cont_klartext = "*",			 #  Finns: "Sjukpenning", "Sjuk- och aktivitetsersersättning", "Arbetslöshetsersättning", "Arbetsmarknadsåtgärder", "Ekonomiskt bistånd", "Etableringsersättning", "Summa helårsekvivalenter", "Folkmängd", "Andel av befolkningen"
+			tid_koder = "*",			 # "*" = alla år eller månader, "9999" = senaste, finns: "2014M01", "2014M02", "2014M03", "2014M04", "2014M05", "2014M06", "2014M07", "2014M08", "2014M09", "2014M10", "2014M11", "2014M12", "2015M01", "2015M02", "2015M03", "2015M04", "2015M05", "2015M06", "2015M07", "2015M08", "2015M09", "2015M10", "2015M11", "2015M12", "2016M01", "2016M02", "2016M03", "2016M04", "2016M05", "2016M06", "2016M07", "2016M08", "2016M09", "2016M10", "2016M11", "2016M12", "2017M01", "2017M02", "2017M03", "2017M04", "2017M05", "2017M06", "2017M07", "2017M08", "2017M09", "2017M10", "2017M11", "2017M12", "2018M01", "2018M02", "2018M03", "2018M04", "2018M05", "2018M06", "2018M07", "2018M08", "2018M09", "2018M10", "2018M11", "2018M12", "2019M01", "2019M02", "2019M03", "2019M04", "2019M05", "2019M06", "2019M07", "2019M08", "2019M09", "2019M10", "2019M11", "2019M12", "2020M01", "2020M02", "2020M03", "2020M04", "2020M05", "2020M06", "2020M07", "2020M08", "2020M09", "2020M10", "2020M11", "2020M12", "2021M01", "2021M02", "2021M03", "2021M04", "2021M05", "2021M06", "2021M07", "2021M08", "2021M09", "2021M10", "2021M11", "2021M12", "2022M01", "2022M02", "2022M03", "2022M04", "2022M05", "2022M06", "2022M07", "2022M08", "2022M09", "2022M10", "2022M11", "2022M12", "2023M01", "2023M02", "2023M03", "2023M04", "2023M05", "2023M06", "2023M07", "2023M08", "2023M09", "2023M10", "2023M11", "2023M12"
+			long_format = TRUE,			# TRUE = konvertera innehållsvariablerna i datasetet till long-format 
+			wide_om_en_contvar = TRUE,			# TRUE = om man vill behålla wide-format om det bara finns en innehållsvariabel, FALSE om man vill konvertera till long-format även om det bara finns en innehållsvariabel
+			output_mapp = NA,			# anges om man vill exportera en excelfil med uttaget, den mapp man vill spara excelfilen till
+			excel_filnamn = "helarsekvivalenter.xlsx",			# filnamn för excelfil som exporteras om excel_filnamn och output_mapp anges
+			returnera_df = TRUE			# TRUE om man vill ha en dataframe i retur från funktionen
+){
+
+  # ====================================================================================================
+  #
+  # Funktion för att hämta data från SCB:s API med hjälp av pxweb-paketet
+  # Automatgenererat av en funktion i R som skrivits av Peter Möller, Region Dalarna
+  #
+  # Skapad av: moepet den 26 mars 2024
+  # Senast uppdaterad: 26 mars 2024
+  #
+  # url till tabellens API: https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__HE__HE0000/HE0000T02N2/
+  #
+  # ====================================================================================================
+
+if (!require("pacman")) install.packages("pacman")
+  p_load(pxweb,
+    			tidyverse,
+    			writexl)
+
+  # Behändiga funktioner som används i skriptet
+  source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R")
+
+  # Url till SCB:s databas
+  url_uttag <- "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/HE/HE0000/HE0000T02N2"
+  px_meta <- pxweb_get(url_uttag)
+
+  varlist_koder <- pxvarlist(px_meta)$koder
+  varlist_bada <- pxvarlist(px_meta)
+
+  # Gör om från klartext till kod som databasen förstår
+  kon_vekt <- if (!all(is.na(kon_klartext))) hamta_kod_med_klartext(px_meta, kon_klartext, skickad_fran_variabel = "kon") else NA
+  aldersgrupp_vekt <- hamta_kod_med_klartext(px_meta, aldersgrupp_klartext, skickad_fran_variabel = "aldersgrupp")
+
+
+  cont_vekt <-  hamta_kod_med_klartext(px_meta, cont_klartext, "contentscode")
+  if (length(cont_vekt) > 1) wide_om_en_contvar <- FALSE
+
+  # Hantera tid-koder
+  giltiga_ar <- hamta_giltiga_varden_fran_tabell(px_meta, "tid")
+  if (all(tid_koder != "*")) tid_koder <- tid_koder %>% as.character() %>% str_replace("9999", max(giltiga_ar)) %>% .[. %in% giltiga_ar] %>% unique()
+
+# query-lista till pxweb-uttag
+  varlista <- list(
+  "Region" = region_vekt,
+  "Kon" = kon_vekt,
+  "Aldersgrupp" = aldersgrupp_vekt,
+  "ContentsCode" = cont_vekt,
+  "Tid" = tid_koder)
+
+  if (all(is.na(kon_klartext))) varlista <- varlista[names(varlista) != "Kon"]
+
+  px_uttag <- pxweb_get(url = url_uttag, query = varlista)
+
+var_vektor <- c(regionkod = "Region")
+var_vektor_klartext <- "region"
+  px_df <- as.data.frame(px_uttag)
+  if (!is.na(var_vektor)) {      px_df <- px_df %>%
+            cbind(as.data.frame(px_uttag, column.name.type = "code", variable.value.type = "code") %>%
+            select(any_of(var_vektor)))
+
+      px_df <- map2(names(var_vektor), var_vektor_klartext, ~ px_df %>% relocate(all_of(.x), .before = all_of(.y))) %>% list_rbind()
+  }
+
+  # man kan välja bort long-format, då låter vi kolumnerna vara wide om det finns fler innehållsvariabler, annars
+  # pivoterar vi om till long-format, dock ej om det bara finns en innehållsvariabel
+  if (long_format & !wide_om_en_contvar) px_df <- px_df %>% konvertera_till_long_for_contentscode_variabler(url_uttag)
+
+
+  # Om användaren vill spara data till en Excel-fil
+  if (!is.na(output_mapp) & !is.na(excel_filnamn)){
+    write.xlsx(px_df, paste0(output_mapp, excel_filnamn))
+  }
+
+  # Returnera data som en dataframe om användern valt det
+  if (returnera_df) return(px_df)
+
+}
