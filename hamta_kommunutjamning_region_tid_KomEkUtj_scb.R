145cab4c7ae20b929820a78cfd4e46f7760babe8
Peter Möller <larspetermoller@gmail.com>
2024-10-15 15:49:11 +0200

    Ny hämta data-funktion. Lediga jobb från RMI.

--- hamta_kommunutjamning_region_tid_KomEkUtj_scb.R
+++ hamta_kommunutjamning_region_tid_KomEkUtj_scb.R
@@ -1,6 +1,6 @@
 hamta_kommunutjamning_region_tid_scb <- function(
-			region_vekt = "*",			# Val av region. Finns: "0114", "0115", "0117", "0120", "0123", "0125", "0126", "0127", "0128", "0136", "0138", "0139", "0140", "0160", "0162", "0163", "0180", "0181", "0182", "0183", "0184", "0186", "0187", "0188", "0191", "0192", "01L", "0305", "0319", "0330", "0331", "0360", "0380", "0381", "0382", "03L", "0428", "0461", "0480", "0481", "0482", "0483", "0484", "0486", "0488", "04L", "0509", "0512", "0513", "0560", "0561", "0562", "0563", "0580", "0581", "0582", "0583", "0584", "0586", "05L", "0604", "0617", "0642", "0643", "0662", "0665", "0680", "0682", "0683", "0684", "0685", "0686", "0687", "06L", "0760", "0761", "0763", "0764", "0765", "0767", "0780", "0781", "07L", "0821", "0834", "0840", "0860", "0861", "0862", "0880", "0881", "0882", "0883", "0884", "0885", "08L", "0980", "0980L", "1060", "1080", "1081", "1082", "1083", "10L", "1214", "1230", "1231", "1233", "1256", "1257", "1260", "1261", "1262", "1263", "1264", "1265", "1266", "1267", "1270", "1272", "1273", "1275", "1276", "1277", "1278", "1280", "1281", "1282", "1283", "1284", "1285", "1286", "1287", "1290", "1291", "1292", "1293", "12L", "1315", "1380", "1381", "1382", "1383", "1384", "13L", "1401", "1402", "1407", "1415", "1419", "1421", "1427", "1430", "1435", "1438", "1439", "1440", "1441", "1442", "1443", "1444", "1445", "1446", "1447", "1452", "1460", "1461", "1462", "1463", "1465", "1466", "1470", "1471", "1472", "1473", "1480", "1481", "1482", "1484", "1485", "1486", "1487", "1488", "1489", "1490", "1491", "1492", "1493", "1494", "1495", "1496", "1497", "1498", "1499", "14L", "1715", "1730", "1737", "1760", "1761", "1762", "1763", "1764", "1765", "1766", "1780", "1781", "1782", "1783", "1784", "1785", "17L", "1814", "1860", "1861", "1862", "1863", "1864", "1880", "1881", "1882", "1883", "1884", "1885", "18L", "1904", "1907", "1960", "1961", "1962", "1980", "1981", "1982", "1983", "1984", "19L", "2021", "2023", "2026", "2029", "2031", "2034", "2039", "2061", "2062", "2080", "2081", "2082", "2083", "2084", "2085", "20L", "2101", "2104", "2121", "2132", "2161", "2180", "2181", "2182", "2183", "2184", "21L", "2260", "2262", "2280", "2281", "2282", "2283", "2284", "22L", "2303", "2305", "2309", "2313", "2321", "2326", "2361", "2380", "23L", "2401", "2403", "2404", "2409", "2417", "2418", "2421", "2422", "2425", "2460", "2462", "2463", "2480", "2481", "2482", "24L", "2505", "2506", "2510", "2513", "2514", "2518", "2521", "2523", "2560", "2580", "2581", "2582", "2583", "2584", "25L"
-			cont_klartext = "*",			 #  Finns: "Inkomstutjämning, kr/invånare", "Kostnadsutjämning, kr/invånare", "Strukturbidrag, kr/invånare", "Införandebidrag, kr/invånare", "Regleringsbidrag/avgift, kr/invånare", "Kommunalekonomisk utjämning, utfall kr/invånare", "Kommunalekonomisk utjämning, utfall, tkr"
+			region_vekt = "*",			   # Val av region. Finns: "0114", "0115", "0117", "0120", "0123", "0125", "0126", "0127", "0128", "0136", "0138", "0139", "0140", "0160", "0162", "0163", "0180", "0181", "0182", "0183", "0184", "0186", "0187", "0188", "0191", "0192", "01", "0305", "0319", "0330", "0331", "0360", "0380", "0381", "0382", "03", "0428", "0461", "0480", "0481", "0482", "0483", "0484", "0486", "0488", "04", "0509", "0512", "0513", "0560", "0561", "0562", "0563", "0580", "0581", "0582", "0583", "0584", "0586", "05", "0604", "0617", "0642", "0643", "0662", "0665", "0680", "0682", "0683", "0684", "0685", "0686", "0687", "06", "0760", "0761", "0763", "0764", "0765", "0767", "0780", "0781", "07", "0821", "0834", "0840", "0860", "0861", "0862", "0880", "0881", "0882", "0883", "0884", "0885", "08", "0980", "0980", "1060", "1080", "1081", "1082", "1083", "10", "1214", "1230", "1231", "1233", "1256", "1257", "1260", "1261", "1262", "1263", "1264", "1265", "1266", "1267", "1270", "1272", "1273", "1275", "1276", "1277", "1278", "1280", "1281", "1282", "1283", "1284", "1285", "1286", "1287", "1290", "1291", "1292", "1293", "12", "1315", "1380", "1381", "1382", "1383", "1384", "13", "1401", "1402", "1407", "1415", "1419", "1421", "1427", "1430", "1435", "1438", "1439", "1440", "1441", "1442", "1443", "1444", "1445", "1446", "1447", "1452", "1460", "1461", "1462", "1463", "1465", "1466", "1470", "1471", "1472", "1473", "1480", "1481", "1482", "1484", "1485", "1486", "1487", "1488", "1489", "1490", "1491", "1492", "1493", "1494", "1495", "1496", "1497", "1498", "1499", "14", "1715", "1730", "1737", "1760", "1761", "1762", "1763", "1764", "1765", "1766", "1780", "1781", "1782", "1783", "1784", "1785", "17", "1814", "1860", "1861", "1862", "1863", "1864", "1880", "1881", "1882", "1883", "1884", "1885", "18", "1904", "1907", "1960", "1961", "1962", "1980", "1981", "1982", "1983", "1984", "19", "2021", "2023", "2026", "2029", "2031", "2034", "2039", "2061", "2062", "2080", "2081", "2082", "2083", "2084", "2085", "20", "2101", "2104", "2121", "2132", "2161", "2180", "2181", "2182", "2183", "2184", "21", "2260", "2262", "2280", "2281", "2282", "2283", "2284", "22", "2303", "2305", "2309", "2313", "2321", "2326", "2361", "2380", "23", "2401", "2403", "2404", "2409", "2417", "2418", "2421", "2422", "2425", "2460", "2462", "2463", "2480", "2481", "2482", "24", "2505", "2506", "2510", "2513", "2514", "2518", "2521", "2523", "2560", "2580", "2581", "2582", "2583", "2584", "25"
+			cont_klartext = "Kommunalekonomisk utjämning, utfall, tkr",			 #  Finns: "Inkomstutjämning, kr/invånare", "Kostnadsutjämning, kr/invånare", "Strukturbidrag, kr/invånare", "Införandebidrag, kr/invånare", "Regleringsbidrag/avgift, kr/invånare", "Kommunalekonomisk utjämning, utfall kr/invånare", "Kommunalekonomisk utjämning, utfall, tkr"
 			tid_koder = "*",			 # "*" = alla år eller månader, "9999" = senaste, finns: "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024"
 			long_format = TRUE,			# TRUE = konvertera innehållsvariablerna i datasetet till long-format 
 			wide_om_en_contvar = TRUE,			# TRUE = om man vill behålla wide-format om det bara finns en innehållsvariabel, FALSE om man vill konvertera till long-format även om det bara finns en innehållsvariabel
@@ -14,8 +14,8 @@ hamta_kommunutjamning_region_tid_scb <- function(
   # Funktion för att hämta data från SCB:s API med hjälp av pxweb-paketet
   # Automatgenererat av en funktion i R som skrivits av Peter Möller, Region Dalarna
   #
-  # Skapad av: moepet den 01 juni 2024
-  # Senast uppdaterad: 01 juni 2024
+  # Skapad av: moepet den 13 oktober 2024
+  # Senast uppdaterad: 13 oktober 2024
   #
   # url till tabellens API: https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__OE__OE0115__OE0115A/KomEkUtj/
   #
@@ -29,35 +29,38 @@ hamta_kommunutjamning_region_tid_scb <- function(
   # Behändiga funktioner som används i skriptet
   source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R")
 
+  
+  
   # Url till SCB:s databas
   url_uttag <- "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/OE/OE0115/OE0115A/KomEkUtj"
   px_meta <- pxweb_get(url_uttag)
 
+  # special för att scb lagt till ett L på alla länskoder så gör vi det här och kan använda riktiga länskoder som parameter i funktionen
+  region_vekt <- ifelse(nchar(region_vekt) == 2, paste0(region_vekt, "L"), region_vekt)
+  
   varlist_koder <- pxvarlist(px_meta)$koder
   varlist_bada <- pxvarlist(px_meta)
-
-  # Gör om från klartext till kod som databasen förstår
 
   cont_vekt <-  hamta_kod_med_klartext(px_meta, cont_klartext, "contentscode")
   if (length(cont_vekt) > 1) wide_om_en_contvar <- FALSE
 
   # Hantera tid-koder
   giltiga_ar <- hamta_giltiga_varden_fran_tabell(px_meta, "tid")
-  if (all(tid_koder != "*")) tid_koder <- tid_koder %>% as.character() %>% str_replace("9999", max(giltiga_ar)) %>% .[. %in% giltiga_ar] %>% unique()
+  tid_vekt <- if (all(tid_koder != "*")) tid_koder %>% as.character() %>% str_replace("9999", max(giltiga_ar)) %>% .[. %in% giltiga_ar] %>% unique() else giltiga_ar
 
   # query-lista till pxweb-uttag
   varlista <- list(
-  "Region" = region_vekt,
-  "ContentsCode" = cont_vekt,
-  "Tid" = tid_koder)
+  	"Region" = region_vekt,
+  	"ContentsCode" = cont_vekt,
+  	"Tid" = tid_vekt)
 
-
-
+  # Hämta data med varlista
   px_uttag <- pxweb_get(url = url_uttag, query = varlista)
 
   var_vektor <- c(regionkod = "Region")
   var_vektor_klartext <- "region"
 
+  # gör om pxweb-uttaget till en dataframe
   px_df <- as.data.frame(px_uttag)
   if (!all(is.na(var_vektor))) {
       # om man vill ha med koder också för variabler utöver klartext så läggs de på här (om det finns värden i var_vektor)
@@ -68,10 +71,13 @@ hamta_kommunutjamning_region_tid_scb <- function(
       # kolumnerna med koder läggs framför motsvarande kolumner med klartext
       for (varflytt_index in 1:length(var_vektor)) {
         px_df <- px_df %>%
-            relocate(all_of(names(var_vektor)[varflytt_index]), .before = all_of(var_vektor_klartext[varflytt_index]))
+            relocate(any_of(names(var_vektor)[varflytt_index]), .before = any_of(var_vektor_klartext[varflytt_index]))
       }
   }
 
+  px_df <- px_df %>% 
+    mutate(regionkod = regionkod %>% str_remove("L"))
+  
   # man kan välja bort long-format, då låter vi kolumnerna vara wide om det finns fler innehållsvariabler, annars
   # pivoterar vi om till long-format, dock ej om det bara finns en innehållsvariabel
   if (long_format & !wide_om_en_contvar) px_df <- px_df %>% konvertera_till_long_for_contentscode_variabler(url_uttag)
@@ -83,5 +89,4 @@ hamta_kommunutjamning_region_tid_scb <- function(
 
   # Returnera data som en dataframe om användern valt det
   if (returnera_df) return(px_df)
-
-}
+} # slut hämta data-funktion
---
+++ hamta_lediga_jobb_region_sektor_tid_RegionIndE1K_RegionIndE1KN_scb.R
@@ -0,0 +1,98 @@
+hamta_lediga_jobb_region_sektor_tid_scb <- function(
+			region_vekt = "20",			# Val av region. Finns: "00", "01", "03", "04", "05", "06", "07", "08", "09", "10", "12", "13", "14", "17", "18", "19", "20", "21", "22", "23", "24", "25"
+			sektor_klartext = "*",			 #  Finns: "offentlig sektor", "privat sektor", "totalt", "hela ekonomin", "näringslivet och hushållens icke-vinstdrivande organisationer", "offentlig förvaltning"
+			cont_klartext = "*",			 #  Finns: "Lediga jobb", "Felmarginal ±", "Lediga jobb, totalt", "Lediga jobb, totalt, osäkerhetsmarginal", "Lediga jobb med omgående tillträde", "Lediga jobb med omgående tillträde, osäkerhetsmargnial"
+			tid_koder = "*",			 # "*" = alla år eller månader, "9999" = senaste, finns: "2006K1", "2006K2", "2006K3", "2006K4", "2007K1", "2007K2", "2007K3", "2007K4", "2008K1", "2008K2", "2008K3", "2008K4", "2009K1", "2009K2", "2009K3", "2009K4", "2010K1", "2010K2", "2010K3", "2010K4", "2011K1", "2011K2", "2011K3", "2011K4", "2012K1", "2012K2", "2012K3", "2012K4", "2013K1", "2013K2", "2013K3", "2013K4", "2014K1", "2014K2", "2014K3", "2014K4", "2015K1", "2015K2", "2015K3", "2015K4", "2016K1", "2016K2", "2016K3", "2016K4", "2017K1", "2017K2", "2017K3", "2017K4", "2018K1", "2018K2", "2018K3", "2018K4", "2019K1", "2019K2", "2019K3", "2019K4", "2020K1", "2020K2", "2020K3", "2020K4", "2021K1", "2021K2", "2021K3", "2021K4", "2022K1", "2022K2", "2022K3", "2022K4", "2023K1", "2023K2", "2023K3", "2023K4", "2024K1", "2024K2"
+			long_format = TRUE,			# TRUE = konvertera innehållsvariablerna i datasetet till long-format 
+			wide_om_en_contvar = TRUE,			# TRUE = om man vill behålla wide-format om det bara finns en innehållsvariabel, FALSE om man vill konvertera till long-format även om det bara finns en innehållsvariabel
+			output_mapp = NA,			# anges om man vill exportera en excelfil med uttaget, den mapp man vill spara excelfilen till
+			excel_filnamn = "lediga_jobb.xlsx",			# filnamn för excelfil som exporteras om excel_filnamn och output_mapp anges
+			returnera_df = TRUE			# TRUE om man vill ha en dataframe i retur från funktionen
+){
+
+  # ====================================================================================================
+  #
+  # Funktion för att hämta data från SCB:s API med hjälp av pxweb-paketet
+  # Automatgenererat av en funktion i R som skrivits av Peter Möller, Region Dalarna
+  #
+  # Skapad av: frkjon den 04 oktober 2024
+  # Senast uppdaterad: 04 oktober 2024
+  #
+  # url till tabellens API: https://api.scb.se/OV0104/v1/doris/sv/ssd/START/AM/AM9906/AM9906O/RegionIndE1K
+  #												https://api.scb.se/OV0104/v1/doris/sv/ssd/START/AM/AM9906/AM9906A/RegionIndE1KN
+  #
+  # ====================================================================================================
+
+  if (!require("pacman")) install.packages("pacman")
+  p_load(pxweb,
+    			tidyverse,
+    			writexl)
+
+  # Behändiga funktioner som används i skriptet
+  source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R")
+
+  # Url till SCB:s databas
+  url_list <- c("https://api.scb.se/OV0104/v1/doris/sv/ssd/START/AM/AM9906/AM9906O/RegionIndE1K",
+						"https://api.scb.se/OV0104/v1/doris/sv/ssd/START/AM/AM9906/AM9906A/RegionIndE1KN")
+
+ hamta_data <- function(url_uttag) {
+
+  px_meta <- pxweb_get(url_uttag)
+
+  varlist_koder <- pxvarlist(px_meta)$koder
+  varlist_bada <- pxvarlist(px_meta)
+
+  # Gör om från klartext till kod som databasen förstår
+  sektor_vekt <- hamta_kod_med_klartext(px_meta, sektor_klartext, skickad_fran_variabel = "sektor")
+
+  cont_vekt <-  hamta_kod_med_klartext(px_meta, cont_klartext, "contentscode")
+  if (length(cont_vekt) > 1) wide_om_en_contvar <- FALSE
+
+  # Hantera tid-koder
+  giltiga_ar <- hamta_giltiga_varden_fran_tabell(px_meta, "tid")
+  tid_vekt <- if (all(tid_koder != "*")) tid_koder %>% as.character() %>% str_replace("9999", max(giltiga_ar)) %>% .[. %in% giltiga_ar] %>% unique() else giltiga_ar
+
+  # query-lista till pxweb-uttag
+  varlista <- list(
+  	"Region" = region_vekt,
+  	"Sektor" = sektor_vekt,
+  	"ContentsCode" = cont_vekt,
+  	"Tid" = tid_vekt)
+
+  px_uttag <- pxweb_get(url = url_uttag, query = varlista)
+
+  var_vektor <- c(regionkod = "Region")
+  var_vektor_klartext <- "region"
+
+  px_df <- as.data.frame(px_uttag)
+  if (!all(is.na(var_vektor))) {
+      # om man vill ha med koder också för variabler utöver klartext så läggs de på här (om det finns värden i var_vektor)
+      px_df <- px_df %>%
+            cbind(as.data.frame(px_uttag, column.name.type = "code", variable.value.type = "code") %>%
+            select(any_of(var_vektor)))
+
+      # kolumnerna med koder läggs framför motsvarande kolumner med klartext
+      for (varflytt_index in 1:length(var_vektor)) {
+        px_df <- px_df %>%
+            relocate(any_of(names(var_vektor)[varflytt_index]), .before = any_of(var_vektor_klartext[varflytt_index]))
+      }
+  }
+
+  # man kan välja bort long-format, då låter vi kolumnerna vara wide om det finns fler innehållsvariabler, annars
+  # pivoterar vi om till long-format, dock ej om det bara finns en innehållsvariabel
+  if (long_format & !wide_om_en_contvar) px_df <- px_df %>% konvertera_till_long_for_contentscode_variabler(url_uttag)
+
+  return(px_df)
+  } # slut hämta data-funktion 
+
+  px_alla <- map(url_list, ~ hamta_data(.x)) %>% list_rbind()
+
+  # Om användaren vill spara data till en Excel-fil
+  if (!is.na(output_mapp) & !is.na(excel_filnamn)){
+    write.xlsx(px_alla, paste0(output_mapp, excel_filnamn))
+  }
+
+  # Returnera data som en dataframe om användern valt det
+  if (returnera_df) return(px_alla)
+
+}
