0269f69ae663a6689d7f501b6c8aad00584e0ee6
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2025-01-10 09:15:18 +0100

    Nytt skript sysselsatta bransch bas på årsbasis

---
+++ hamta_sysselsatta_bas_region_kon_sni2007_utbildningsniva_tid_ArRegUtb_AM0210F_scb.R
@@ -0,0 +1,98 @@
+hamta_sysselsatta_region_kon_sni2007_utbildningsniva_tid_scb <- function(
+    region_vekt = "20",			   # Val av region. Finns: "00", "01", "0114", "0115", "0117", "0120", "0123", "0125", "0126", "0127", "0128", "0136", "0138", "0139", "0140", "0160", "0162", "0163", "0180", "0181", "0182", "0183", "0184", "0186", "0187", "0188", "0191", "0192", "03", "0305", "0319", "0330", "0331", "0360", "0380", "0381", "0382", "04", "0428", "0461", "0480", "0481", "0482", "0483", "0484", "0486", "0488", "05", "0509", "0512", "0513", "0560", "0561", "0562", "0563", "0580", "0581", "0582", "0583", "0584", "0586", "06", "0604", "0617", "0642", "0643", "0662", "0665", "0680", "0682", "0683", "0684", "0685", "0686", "0687", "07", "0760", "0761", "0763", "0764", "0765", "0767", "0780", "0781", "08", "0821", "0834", "0840", "0860", "0861", "0862", "0880", "0881", "0882", "0883", "0884", "0885", "09", "0980", "10", "1060", "1080", "1081", "1082", "1083", "12", "1214", "1230", "1231", "1233", "1256", "1257", "1260", "1261", "1262", "1263", "1264", "1265", "1266", "1267", "1270", "1272", "1273", "1275", "1276", "1277", "1278", "1280", "1281", "1282", "1283", "1284", "1285", "1286", "1287", "1290", "1291", "1292", "1293", "13", "1315", "1380", "1381", "1382", "1383", "1384", "14", "1401", "1402", "1407", "1415", "1419", "1421", "1427", "1430", "1435", "1438", "1439", "1440", "1441", "1442", "1443", "1444", "1445", "1446", "1447", "1452", "1460", "1461", "1462", "1463", "1465", "1466", "1470", "1471", "1472", "1473", "1480", "1481", "1482", "1484", "1485", "1486", "1487", "1488", "1489", "1490", "1491", "1492", "1493", "1494", "1495", "1496", "1497", "1498", "1499", "17", "1715", "1730", "1737", "1760", "1761", "1762", "1763", "1764", "1765", "1766", "1780", "1781", "1782", "1783", "1784", "1785", "18", "1814", "1860", "1861", "1862", "1863", "1864", "1880", "1881", "1882", "1883", "1884", "1885", "19", "1904", "1907", "1960", "1961", "1962", "1980", "1981", "1982", "1983", "1984", "20", "2021", "2023", "2026", "2029", "2031", "2034", "2039", "2061", "2062", "2080", "2081", "2082", "2083", "2084", "2085", "21", "2101", "2104", "2121", "2132", "2161", "2180", "2181", "2182", "2183", "2184", "22", "2260", "2262", "2280", "2281", "2282", "2283", "2284", "23", "2303", "2305", "2309", "2313", "2321", "2326", "2361", "2380", "24", "2401", "2403", "2404", "2409", "2417", "2418", "2421", "2422", "2425", "2460", "2462", "2463", "2480", "2481", "2482", "25", "2505", "2506", "2510", "2513", "2514", "2518", "2521", "2523", "2560", "2580", "2581", "2582", "2583", "2584"
+    kon_klartext = "*",			 #  NA = tas inte med i uttaget,  Finns: "män", "kvinnor", "totalt"
+    sni2007_klartext = "*",			 #  NA = tas inte med i uttaget,  Finns: "Total", "företag inom jordbruk, skogsbruk och fiske", "tillverkningsindustri; gruvor och mineralutvinningsindustri", "företag inom energi och miljö", "byggindustri", "handel; serviceverkstäder för motorfordon och motorcyklar", "transport- och magasineringsföretag", "hotell och restauranger", "informations- och kommunikationsföretag", "kreditinstitut och försäkringsbolag m.m.", "fastighetsbolag och fastighetsförvaltare", "företag inom juridik, ekonomi, vetenskap och teknik; företag inom uthyrning, fastighetsservice, resetjänster och andra stödtjänster", "civila myndigheter och försvaret", "utbildningsväsendet", "enheter för vård och omsorg, socialtjänst", "enheter för kultur, nöje och fritid; andra serviceföretag m.m.", "uppgift saknas"
+    utbildningsniva_klartext = "*",			 #  Finns: "förgymnasial utbildning", "gymnasial utbildning, högst 2 år", "gymnasial utbildning, 3 år", "eftergymnasial utbildning, mindre än 3 år", "eftergymnasial utbildning 3 år eller längre inkl. forskarutbildning", "uppgift om utbildningsnivå saknas"
+    cont_klartext = "*",			 #  Finns: "sysselsatta efter arbetsställets belägenhet", "sysselsatta efter bostadens belägenhet"
+    tid_koder = "*",			 # "*" = alla år eller månader, "9999" = senaste, finns: "2020", "2021", "2022", "2023"
+    long_format = TRUE,			# TRUE = konvertera innehållsvariablerna i datasetet till long-format 
+    wide_om_en_contvar = TRUE,			# TRUE = om man vill behålla wide-format om det bara finns en innehållsvariabel, FALSE om man vill konvertera till long-format även om det bara finns en innehållsvariabel
+    output_mapp = NA,			# anges om man vill exportera en excelfil med uttaget, den mapp man vill spara excelfilen till
+    excel_filnamn = "sysselsatta.xlsx",			# filnamn för excelfil som exporteras om excel_filnamn och output_mapp anges
+    returnera_df = TRUE			# TRUE om man vill ha en dataframe i retur från funktionen
+){
+  
+  # ====================================================================================================
+  #
+  # Funktion för att hämta data från SCB:s API med hjälp av pxweb-paketet
+  # Automatgenererat av en funktion i R som skrivits av Peter Möller, Region Dalarna
+  #
+  # Skapad av: frkjon den 10 januari 2025
+  # Senast uppdaterad: 10 januari 2025
+  #
+  # url till tabellens API: https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__AM__AM0210__AM0210F/ArRegUtb/
+  #
+  # ====================================================================================================
+  
+  if (!require("pacman")) install.packages("pacman")
+  p_load(pxweb,
+         tidyverse,
+         writexl)
+  
+  # Behändiga funktioner som används i skriptet
+  source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R")
+  
+  # Url till databas
+  url_uttag <- "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/AM/AM0210/AM0210F/ArRegUtb"
+  px_meta <- pxweb_get(url_uttag)
+  
+  varlist_koder <- pxvarlist(px_meta)$koder
+  varlist_bada <- pxvarlist(px_meta)
+  
+  # Gör om från klartext till kod som databasen förstår
+  kon_vekt <- if (!all(is.na(kon_klartext))) hamta_kod_med_klartext(px_meta, kon_klartext, skickad_fran_variabel = "kon") else NA
+  sni2007_vekt <- if (!all(is.na(sni2007_klartext))) hamta_kod_med_klartext(px_meta, sni2007_klartext, skickad_fran_variabel = "sni2007") else NA
+  utbildningsniva_vekt <- hamta_kod_med_klartext(px_meta, utbildningsniva_klartext, skickad_fran_variabel = "utbildningsniva")
+  
+  cont_vekt <-  hamta_kod_med_klartext(px_meta, cont_klartext, "contentscode")
+  if (length(cont_vekt) > 1) wide_om_en_contvar <- FALSE
+  
+  # Hantera tid-koder
+  giltiga_ar <- hamta_giltiga_varden_fran_tabell(px_meta, "tid")
+  tid_vekt <- if (all(tid_koder != "*")) tid_koder %>% as.character() %>% str_replace("9999", max(giltiga_ar)) %>% .[. %in% giltiga_ar] %>% unique() else giltiga_ar
+  
+  # query-lista till pxweb-uttag
+  varlista <- list(
+    "Region" = region_vekt,
+    "Kon" = kon_vekt,
+    "SNI2007" = sni2007_vekt,
+    "UtbildningsNiva" = utbildningsniva_vekt,
+    "ContentsCode" = cont_vekt,
+    "Tid" = tid_vekt)
+  
+  if (all(is.na(kon_klartext))) varlista <- varlista[names(varlista) != "Kon"]
+  if (all(is.na(sni2007_klartext))) varlista <- varlista[names(varlista) != "SNI2007"]
+  
+  # Hämta data med varlista
+  px_uttag <- pxweb_get(url = url_uttag, query = varlista)
+  
+  var_vektor <- c(regionkod = "Region")
+  var_vektor_klartext <- "region"
+  
+  # gör om pxweb-uttaget till en dataframe
+  px_df <- as.data.frame(px_uttag)
+  if (!all(is.na(var_vektor))) {
+    # om man vill ha med koder också för variabler utöver klartext så läggs de på här (om det finns värden i var_vektor)
+    px_df <- px_df %>%
+      cbind(as.data.frame(px_uttag, column.name.type = "code", variable.value.type = "code") %>%
+              select(any_of(var_vektor)))
+    
+    # kolumnerna med koder läggs framför motsvarande kolumner med klartext
+    for (varflytt_index in 1:length(var_vektor)) {
+      px_df <- px_df %>%
+        relocate(any_of(names(var_vektor)[varflytt_index]), .before = any_of(var_vektor_klartext[varflytt_index]))
+    }
+  }
+  
+  # man kan välja bort long-format, då låter vi kolumnerna vara wide om det finns fler innehållsvariabler, annars
+  # pivoterar vi om till long-format, dock ej om det bara finns en innehållsvariabel
+  if (long_format & !wide_om_en_contvar) px_df <- px_df %>% konvertera_till_long_for_contentscode_variabler(url_uttag)
+  
+  # Om användaren vill spara data till en Excel-fil
+  if (!is.na(output_mapp) & !is.na(excel_filnamn)){
+    write.xlsx(px_df, paste0(output_mapp, excel_filnamn))
+  }
+  
+  # Returnera data som en dataframe om användern valt det
+  if (returnera_df) return(px_df)
+} # slut hämta data-funktion
