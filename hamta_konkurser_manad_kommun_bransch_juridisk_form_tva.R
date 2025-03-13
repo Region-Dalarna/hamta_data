5c5e6ae4779650f3791612959d99e5b46feaff7a
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2025-03-13 13:56:15 +0100

    Justerat felaktighet i skript

--- hamta_konkurser_manad_kommun_bransch_juridisk_form_tva.R
+++ hamta_konkurser_manad_kommun_bransch_juridisk_form_tva.R
@@ -18,7 +18,9 @@ hamta_konkurser_region_bransch_juridisk_form_tva <- function(
   # Senast uppdaterad: 19 februari 2025
   #
   # url till tabellens API: https://statistik.tillvaxtanalys.se:443/PxWeb/api/v1/sv/Tillväxtanalys statistikdatabas/Konkurser och offentliga ackord/z_anstallda_manad_kom_bransch_juridisk_form_popAH_20250212_140613.px
-  # Enligt Tillväxtanalys är det prliminär statistik som på sikt kan utäkas med flera typer av juridisk form.
+  # Enligt Tillväxtanalys är det preliminär statistik som på sikt kan utökas med flera typer av juridisk form.
+  # Av oklar anledning hade url-adressen ändrats till 
+  # https://statistik.tillvaxtanalys.se:443/PxWeb/api/v1/sv/Tillväxtanalys statistikdatabas/Konkurser och offentliga ackord/z_anstallda_manad_kom_bransch_juridisk_form_popAH_20250307_081423.px
   # ====================================================================================================
 
   if (!require("pacman")) install.packages("pacman")
@@ -30,7 +32,7 @@ hamta_konkurser_region_bransch_juridisk_form_tva <- function(
   source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R")
 
   # Url till databas
-  url_uttag <- "https://statistik.tillvaxtanalys.se:443:443/PxWeb/api/v1/sv/Tillväxtanalys statistikdatabas/Konkurser och offentliga ackord/z_anstallda_manad_kom_bransch_juridisk_form_popAH_20250212_140613.px"
+  url_uttag <- "https://statistik.tillvaxtanalys.se:443/PxWeb/api/v1/sv/Tillväxtanalys statistikdatabas/Konkurser och offentliga ackord/z_anstallda_manad_kom_bransch_juridisk_form_popAH_20250307_081423.px"
   px_meta <- pxweb_get(url_uttag)
 
   varlist_koder <- pxvarlist(px_meta)$koder
