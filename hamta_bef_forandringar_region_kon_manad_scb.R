c19efcc576f61484bc826dc264de77f432b9c387
Peter Möller <larspetermoller@gmail.com>
2025-05-19 16:58:11 +0200

    Uppdaterat hämta befolkningsförändringar så att CKM-tabellen from år 2025 sätts ihop med tidigare tabell. Gäller månadsdata.

--- hamta_bef_forandringar_region_kon_manad_scb.R
+++ hamta_bef_forandringar_region_kon_manad_scb.R
@@ -1,89 +1,113 @@
-if (!require("pacman")) install.packages("pacman")
-p_load(pxweb,
-       tidyverse)
+hamta_bef_forandringar_region_forandringar_kon_tid_scb <- function(
+			region_vekt = "20",			   # Val av region. Finns: "00", "01", "0114", "0115", "0117", "0120", "0123", "0125", "0126", "0127", "0128", "0136", "0138", "0139", "0140", "0160", "0162", "0163", "0180", "0181", "0182", "0183", "0184", "0186", "0187", "0188", "0191", "0192", "03", "0305", "0319", "0330", "0331", "0360", "0380", "0381", "0382", "04", "0428", "0461", "0480", "0481", "0482", "0483", "0484", "0486", "0488", "05", "0509", "0512", "0513", "0560", "0561", "0562", "0563", "0580", "0581", "0582", "0583", "0584", "0586", "06", "0604", "0617", "0642", "0643", "0662", "0665", "0680", "0682", "0683", "0684", "0685", "0686", "0687", "07", "0760", "0761", "0763", "0764", "0765", "0767", "0780", "0781", "08", "0821", "0834", "0840", "0860", "0861", "0862", "0880", "0881", "0882", "0883", "0884", "0885", "09", "0980", "10", "1060", "1080", "1081", "1082", "1083", "12", "1214", "1230", "1231", "1233", "1256", "1257", "1260", "1261", "1262", "1263", "1264", "1265", "1266", "1267", "1270", "1272", "1273", "1275", "1276", "1277", "1278", "1280", "1281", "1282", "1283", "1284", "1285", "1286", "1287", "1290", "1291", "1292", "1293", "13", "1315", "1380", "1381", "1382", "1383", "1384", "14", "1401", "1402", "1407", "1415", "1419", "1421", "1427", "1430", "1435", "1438", "1439", "1440", "1441", "1442", "1443", "1444", "1445", "1446", "1447", "1452", "1460", "1461", "1462", "1463", "1465", "1466", "1470", "1471", "1472", "1473", "1480", "1481", "1482", "1484", "1485", "1486", "1487", "1488", "1489", "1490", "1491", "1492", "1493", "1494", "1495", "1496", "1497", "1498", "1499", "17", "1715", "1730", "1737", "1760", "1761", "1762", "1763", "1764", "1765", "1766", "1780", "1781", "1782", "1783", "1784", "1785", "18", "1814", "1860", "1861", "1862", "1863", "1864", "1880", "1881", "1882", "1883", "1884", "1885", "19", "1904", "1907", "1960", "1961", "1962", "1980", "1981", "1982", "1983", "1984", "20", "2021", "2023", "2026", "2029", "2031", "2034", "2039", "2061", "2062", "2080", "2081", "2082", "2083", "2084", "2085", "21", "2101", "2104", "2121", "2132", "2161", "2180", "2181", "2182", "2183", "2184", "22", "2260", "2262", "2280", "2281", "2282", "2283", "2284", "23", "2303", "2305", "2309", "2313", "2321", "2326", "2361", "2380", "24", "2401", "2403", "2404", "2409", "2417", "2418", "2421", "2422", "2425", "2460", "2462", "2463", "2480", "2481", "2482", "25", "2505", "2506", "2510", "2513", "2514", "2518", "2521", "2523", "2560", "2580", "2581", "2582", "2583", "2584"
+			befforandr_klartext = "*",			 #  Finns: "folkmängd", "folkökning", "födda", "döda", "födelseöverskott", "samtliga inflyttningar", "samtliga utflyttningar", "samtliga inrikes inflyttningar", "inflyttningar från kommuner inom länet", "inflyttningar från övriga län", "invandringar", "samtliga inrikes utflyttningar", "utflyttningar till kommuner inom länet", "utflyttningar till övriga län", "utvandringar", "flyttningsöverskott totalt", "flyttningsöverskott inrikes totalt", "flyttningsöverskott eget län", "flyttningsöverskott övriga Sverige", "invandringsöverskott", "justeringspost"
+			kon_klartext = "*",			 #  NA = tas inte med i uttaget,  Finns: "män", "kvinnor", "totalt", "totalt, samtliga män och kvinnor"
+			tid_koder = "*",			 # "*" = alla år eller månader, "9999" = senaste, finns: "2000M01", "2000M02", "2000M03", "2000M04", "2000M05", "2000M06", "2000M07", "2000M08", "2000M09", "2000M10", "2000M11", "2000M12", "2001M01", "2001M02", "2001M03", "2001M04", "2001M05", "2001M06", "2001M07", "2001M08", "2001M09", "2001M10", "2001M11", "2001M12", "2002M01", "2002M02", "2002M03", "2002M04", "2002M05", "2002M06", "2002M07", "2002M08", "2002M09", "2002M10", "2002M11", "2002M12", "2003M01", "2003M02", "2003M03", "2003M04", "2003M05", "2003M06", "2003M07", "2003M08", "2003M09", "2003M10", "2003M11", "2003M12", "2004M01", "2004M02", "2004M03", "2004M04", "2004M05", "2004M06", "2004M07", "2004M08", "2004M09", "2004M10", "2004M11", "2004M12", "2005M01", "2005M02", "2005M03", "2005M04", "2005M05", "2005M06", "2005M07", "2005M08", "2005M09", "2005M10", "2005M11", "2005M12", "2006M01", "2006M02", "2006M03", "2006M04", "2006M05", "2006M06", "2006M07", "2006M08", "2006M09", "2006M10", "2006M11", "2006M12", "2007M01", "2007M02", "2007M03", "2007M04", "2007M05", "2007M06", "2007M07", "2007M08", "2007M09", "2007M10", "2007M11", "2007M12", "2008M01", "2008M02", "2008M03", "2008M04", "2008M05", "2008M06", "2008M07", "2008M08", "2008M09", "2008M10", "2008M11", "2008M12", "2009M01", "2009M02", "2009M03", "2009M04", "2009M05", "2009M06", "2009M07", "2009M08", "2009M09", "2009M10", "2009M11", "2009M12", "2010M01", "2010M02", "2010M03", "2010M04", "2010M05", "2010M06", "2010M07", "2010M08", "2010M09", "2010M10", "2010M11", "2010M12", "2011M01", "2011M02", "2011M03", "2011M04", "2011M05", "2011M06", "2011M07", "2011M08", "2011M09", "2011M10", "2011M11", "2011M12", "2012M01", "2012M02", "2012M03", "2012M04", "2012M05", "2012M06", "2012M07", "2012M08", "2012M09", "2012M10", "2012M11", "2012M12", "2013M01", "2013M02", "2013M03", "2013M04", "2013M05", "2013M06", "2013M07", "2013M08", "2013M09", "2013M10", "2013M11", "2013M12", "2014M01", "2014M02", "2014M03", "2014M04", "2014M05", "2014M06", "2014M07", "2014M08", "2014M09", "2014M10", "2014M11", "2014M12", "2015M01", "2015M02", "2015M03", "2015M04", "2015M05", "2015M06", "2015M07", "2015M08", "2015M09", "2015M10", "2015M11", "2015M12", "2016M01", "2016M02", "2016M03", "2016M04", "2016M05", "2016M06", "2016M07", "2016M08", "2016M09", "2016M10", "2016M11", "2016M12", "2017M01", "2017M02", "2017M03", "2017M04", "2017M05", "2017M06", "2017M07", "2017M08", "2017M09", "2017M10", "2017M11", "2017M12", "2018M01", "2018M02", "2018M03", "2018M04", "2018M05", "2018M06", "2018M07", "2018M08", "2018M09", "2018M10", "2018M11", "2018M12", "2019M01", "2019M02", "2019M03", "2019M04", "2019M05", "2019M06", "2019M07", "2019M08", "2019M09", "2019M10", "2019M11", "2019M12", "2020M01", "2020M02", "2020M03", "2020M04", "2020M05", "2020M06", "2020M07", "2020M08", "2020M09", "2020M10", "2020M11", "2020M12", "2021M01", "2021M02", "2021M03", "2021M04", "2021M05", "2021M06", "2021M07", "2021M08", "2021M09", "2021M10", "2021M11", "2021M12", "2022M01", "2022M02", "2022M03", "2022M04", "2022M05", "2022M06", "2022M07", "2022M08", "2022M09", "2022M10", "2022M11", "2022M12", "2023M01", "2023M02", "2023M03", "2023M04", "2023M05", "2023M06", "2023M07", "2023M08", "2023M09", "2023M10", "2023M11", "2023M12", "2024M01", "2024M02", "2024M03", "2024M04", "2024M05", "2024M06", "2024M07", "2024M08", "2024M09", "2024M10", "2024M11", "2024M12", "2025M01", "2025M02", "2025M03"
+			long_format = TRUE,			# TRUE = konvertera innehållsvariablerna i datasetet till long-format 
+			wide_om_en_contvar = TRUE,			# TRUE = om man vill behålla wide-format om det bara finns en innehållsvariabel, FALSE om man vill konvertera till long-format även om det bara finns en innehållsvariabel
+			mapp_excelfil = NA,			# anges om man vill exportera en excelfil med uttaget, den mapp man vill spara excelfilen till
+			filnamn_excelfil = "bef_forandringar.xlsx",			# filnamn för excelfil som exporteras om filnamn_excelfil och mapp_excelfil anges
+			returnera_df = TRUE			# TRUE om man vill ha en dataframe i retur från funktionen
+){
 
-source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_SkapaDiagram.R", encoding = "utf-8", echo = FALSE)
-source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R", encoding = "utf-8", echo = FALSE)
-source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_text.R", encoding = "utf-8", echo = FALSE)
-options(dplyr.summarise.inform = FALSE)
-
-
-# funktion för att lista vilka befolkningsförändringsvariabler som finns i tabellen
-lista_beffor_manad_vars <- function(){
-  url_beffor <- "https://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101G/ManadBefStatRegion"
-  beffor_vars <- hamta_giltiga_varden_fran_tabell(url_beffor, "Forandringar", klartext = TRUE)
-  return(beffor_vars)
-}
-
-# här är själva huvudfunktionen
-hamta_befolkningsforandringar_manad <- function(region_vekt = "20",
-                                                befforandr_klartext = c("folkmängd", "folkökning"),    # finns: "folkmängd", "folkökning", "födda", "döda", "födelseöverskott", "samtliga inflyttningar", "samtliga utflyttningar", "samtliga inrikes inflyttningar", "inflyttningar från kommuner inom länet", "inflyttningar från övriga län", "invandringar", "samtliga inrikes utflyttningar", "utflyttningar till kommuner inom länet", "utflyttningar till övriga län", "utvandringar", "flyttningsöverskott totalt", "flyttningsöverskott inrikes totalt", "flyttningsöverskott eget län", "flyttningsöverskott övriga Sverige", "invandringsöverskott", "justeringspost"
-                                                kon_klartext = "totalt",                # finns: "män", "kvinnor", "totalt"
-                                                tid_koder = "*",                         # "9999" = senaste år
-                                                returnera_df = TRUE,                      # FALSE om man inte vill returnera en df
-                                                mapp_excelfil = NA,                       # var man vill spara sin Excelfil om man vill spara en sådan
-                                                filnamn_excelfil = NA,                    # filnamn på sin excelfil 
-                                                long_format = TRUE                       # om man vill formatera till long-format
-) { 
-  
-  # ===========================================================================================================
+  # ====================================================================================================
   #
-  # Skript för att hämta data från SCB för befolkning månadsvis. ContentsCode är enbart ett värde (befolkning)
-  # och därför inte valbar som parameter.
-  # 
-  # Parametrar som skickas med (= variabler i SCB-tabellen) är:
-  # - Innehåll                                                    # Folkmängd (standard) och/eller Folkökning
-  # - Region                                                      # tabellen innehåller kommuner, län och riket, det är regionkoder som skickas med
-  # - Kön                                                         # finns enbart kvinnor och män (inte totalt)
-  # - Förändringar                                                # finns: "folkmängd", "folkökning", "födda", "döda", "födelseöverskott", "samtliga inflyttningar", "samtliga utflyttningar", "samtliga inrikes inflyttningar", "inflyttningar från kommuner inom länet", "inflyttningar från övriga län", "invandringar", "samtliga inrikes utflyttningar", "utflyttningar till kommuner inom länet", "utflyttningar till övriga län", "utvandringar", "flyttningsöverskott totalt", "flyttningsöverskott inrikes totalt", "flyttningsöverskott eget län", "flyttningsöverskott övriga Sverige", "invandringsöverskott", "justeringspost"
-  # - tid (dvs. år)                                               # * = alla år (standard). "9999" = senaste tillgängliga år i tabellen. År från och med 1968
+  # Funktion för att hämta data från SCB:s API med hjälp av pxweb-paketet
+  # Automatgenererat av en funktion i R som skrivits av Peter Möller, Region Dalarna
   #
-  # Skapat av: Peter Möller, Region Dalarna
-  #            februari 2024
-  # Senast uppdaterat:  
-  #                     
-  # ===========================================================================================================
+  # Skapad av: moepet den 19 maj 2025, och ersätter tidigare skript med samma namn, men nu med nya CKM-tabellen så vi sätter
+  #            ihop data från båda tabellerna i en och samma dataframe, dvs data från och med år 2025 med data från tidigare år
+  #
+  # Senast uppdaterad: 19 maj 2025
+  #
+  # url till tabellens API: https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__BE__BE0101__BE0101G/ManadBefStatRegion/
+  #												https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__BE__BE0101__BE0101G/MBefStatRegionCKM/
+  #
+  # ====================================================================================================
 
-  skriv_excelfil <- if (!is.na(mapp_excelfil) & !is.na(filnamn_excelfil)) TRUE else FALSE
-  
-  if (returnera_df | skriv_excelfil) {                       # skriptet körs bara om användaren valt att returnera en dataframe och/eller excelfil
-    
-    url_beffor <- "https://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101G/ManadBefStatRegion"
-    px_meta <- pxweb_get(url = url_beffor)
-    
-    kon_koder <- hamta_kod_eller_klartext_fran_lista(px_meta, kon_klartext, skickad_fran_variabel = "kon")
-    befforandr_koder <- hamta_kod_eller_klartext_fran_lista(px_meta, befforandr_klartext, skickad_fran_variabel = "forandringar")
-    
-    # hantering av tid (i detta fall år) och att kunna skicka med "9999" som senaste år
-    giltiga_ar <- hamta_kod_eller_klartext_fran_lista(px_meta, "*", "tid")
-    tid_koder <- tid_koder %>% 
-      as.character() %>% 
-      str_replace("9999", max(giltiga_ar)) %>%
-      str_replace("\\*", giltiga_ar) %>% 
-      .[. %in% giltiga_ar] %>% 
-      unique()
-    
-    # ================================ API-uttag befolkningsförändringar ===============================================
-    
-    varlista <- list(
-      Region = region_vekt,
-      Kon = kon_koder,
-      Forandringar = befforandr_koder,
-      ContentsCode = "*",
-      Tid = tid_koder
-    )
-  
-    px_uttag <- pxweb_get(url = url_beffor, query = varlista)              # hämta data från pxweb
-    
-    px_df <- suppressWarnings(as.data.frame(px_uttag) %>%                        # spara i en dataframe, plocka med regionkoder 
-                                cbind(as.data.frame(px_uttag, column.name.type = "code", variable.value.type = "code") %>%
-                                        select(Region))) %>% 
-      rename(regionkod = Region) %>% relocate(regionkod, .before = region) %>% 
-      manader_bearbeta_scbtabeller()
-  
-    if (returnera_df) return(px_df)
-    if (skriv_excelfil) write_xlsx(px_df, paste0(mapp_excelfil, filnamn_excelfil))
-    
+  if (!require("pacman")) install.packages("pacman")
+  p_load(pxweb,
+    			tidyverse,
+    			writexl)
 
-  } else print("Varken 'returnera_df' eller mapp och filnamn för excelfil har valts så då körs inte skriptet då inget returneras. Välj någon av dessa eller båda och kör skriptet igen.") # slut if-sats där vi testar att användaren valt att spara en dataframe och/eller Excelfil
-} # slut funktion
+  # Behändiga funktioner som används i skriptet
+  source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R")
+
+  # Url till databas
+  url_list <- c("https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101G/ManadBefStatRegion",
+						"https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101G/MBefStatRegionCKM")
+
+  cont_klartext <- "*"
+  
+ hamta_data <- function(url_uttag) {
+
+  px_meta <- pxweb_get(url_uttag)
+
+  varlist_koder <- pxvarlist(px_meta)$koder
+  varlist_bada <- pxvarlist(px_meta)
+
+  # Gör om från klartext till kod som databasen förstår
+  forandringar_vekt <- hamta_kod_med_klartext(px_meta, befforandr_klartext, skickad_fran_variabel = "forandringar")
+  kon_vekt <- if (!all(is.na(kon_klartext))) hamta_kod_med_klartext(px_meta, kon_klartext, skickad_fran_variabel = "kon") else NA
+
+  cont_vekt <-  hamta_kod_med_klartext(px_meta, cont_klartext, "contentscode")
+  if (length(cont_vekt) > 1) wide_om_en_contvar <- FALSE
+
+  # Hantera tid-koder
+  giltiga_ar <- hamta_giltiga_varden_fran_tabell(px_meta, "tid")
+  tid_vekt <- if (all(tid_koder != "*")) tid_koder %>% as.character() %>% str_replace("9999", max(giltiga_ar)) %>% .[. %in% giltiga_ar] %>% unique() else giltiga_ar
+
+  # query-lista till pxweb-uttag
+  varlista <- list(
+  	"Region" = region_vekt,
+  	"Forandringar" = forandringar_vekt,
+  	"Kon" = kon_vekt,
+  	"ContentsCode" = cont_vekt,
+  	"Tid" = tid_vekt)
+
+  if (all(is.na(kon_klartext))) varlista <- varlista[names(varlista) != "Kon"]
+
+  # Hämta data med varlista
+  px_uttag <- pxweb_get(url = url_uttag, query = varlista)
+
+  var_vektor <- c(regionkod = "Region")
+  var_vektor_klartext <- "region"
+
+  # gör om pxweb-uttaget till en dataframe
+  px_df <- as.data.frame(px_uttag)
+  if (!all(is.na(var_vektor))) {
+      # om man vill ha med koder också för variabler utöver klartext så läggs de på här (om det finns värden i var_vektor)
+      px_df <- px_df %>%
+            cbind(as.data.frame(px_uttag, column.name.type = "code", variable.value.type = "code") %>%
+            select(any_of(var_vektor)))
+
+      # kolumnerna med koder läggs framför motsvarande kolumner med klartext
+      for (varflytt_index in 1:length(var_vektor)) {
+        px_df <- px_df %>%
+            relocate(any_of(names(var_vektor)[varflytt_index]), .before = any_of(var_vektor_klartext[varflytt_index]))
+      }
+  }
+
+  # korrigera att värden för båda könen heter annorlunda i nya tabellen jämfört med den gamla
+  if (!all(is.na(kon_klartext))) {
+    px_df <- px_df %>% 
+      mutate(kön = if_else(str_detect(kön, "totalt"), "totalt", kön))
+  }
+  
+  # man kan välja bort long-format, då låter vi kolumnerna vara wide om det finns fler innehållsvariabler, annars
+  # pivoterar vi om till long-format, dock ej om det bara finns en innehållsvariabel
+  if (long_format & !wide_om_en_contvar) px_df <- px_df %>% konvertera_till_long_for_contentscode_variabler(url_uttag)
+
+  return(px_df)
+  } # slut hämta data-funktion 
+
+  px_alla <- map(url_list, ~ hamta_data(.x)) %>% list_rbind()
+
+  # Om användaren vill spara data till en Excel-fil
+  if (!is.na(mapp_excelfil) & !is.na(filnamn_excelfil)){
+    write.xlsx(px_alla, paste0(mapp_excelfil, filnamn_excelfil))
+  }
+
+  # Returnera data som en dataframe om användern valt det
+  if (returnera_df) return(px_alla)
+} # slut hämta data-funktion
