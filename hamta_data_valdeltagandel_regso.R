b517279bcc32d8e5505334afe994077592130c8d
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2025-09-04 15:51:15 +0200

    Justerat felaktigheter

--- hamta_data_valdeltagandel_regso.R
+++ hamta_data_valdeltagandel_regso.R
@@ -14,6 +14,7 @@ hamta_data_valdeltagande_regso <- function(region_vekt = "20",
                                      output_mapp = NA,                  # Outputmapp. Sätts till en mapp om data skall sparas
                                      filnamn = "valdeltagande_regso.xlsx",  # Filnamn om man vill spara en excelfil i output_mapp.
                                      returnera_data = TRUE,             # Om man vill returnera data som en dataframe från funktionen
+                                     wide_om_en_contvar = TRUE,			# TRUE = om man vill behålla wide-format om det bara finns en innehållsvariabel, FALSE om man vill konvertera till long-format även om det bara finns en innehållsvariabel
                                      long_format = TRUE,                # om man tar med fler än en innehållsvariabel så görs format om från wide till long
                                      tid = "*")                         # Sätts till "9999" om man enbart vill ha senaste år, alternativt ett intervall som slutar 
 # på "9999". "*" ger samtliga år
@@ -31,6 +32,7 @@ hamta_data_valdeltagande_regso <- function(region_vekt = "20",
   # Båda kräver  att source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R") har körts först
   #
   # Skapad av Jon Frank 2024-02-22
+  # Reviderat av Jon Frank 2025-09-04, se hamta_data_ohalsotal_regso för kommentarer om ändringar
   # ===========================================================================================================
   
   if (!require("pacman")) install.packages("pacman")
@@ -53,20 +55,36 @@ hamta_data_valdeltagande_regso <- function(region_vekt = "20",
   # vi tar ut endast RegSO eller DeSO beroende på vad användaren valt
   alla_regionkoder <- if(region_indelning == "RegSO") alla_regionkoder[str_detect(alla_regionkoder, "R")] else alla_regionkoder[str_detect(alla_regionkoder, "A|B|C")]
   
-  if (region_vekt != "*") {
+  if (!("*" %in% region_vekt)) {
     # här delar vi upp de medskickade regionkoderna i RegSO/DeSO, länskoder och kommunkoder 
-    fardiga_regionkoder <- region_vekt[str_length(region_vekt) == 9]           # RegSO eller DeSO
+    fardiga_regionkoder <- region_vekt[str_length(region_vekt) > 7]           # RegSO eller DeSO
     lanskoder <- region_vekt[str_length(region_vekt) == 2]                     # länskoder
     kommunkoder <- region_vekt[str_length(region_vekt) == 4]                   # kommunkoder
     
     # här hämtar vi alla RegSO/DeSO för de länskoder eller kommunkoder som skickats med
     region_lan <- alla_regionkoder[str_sub(alla_regionkoder, 1,2) %in% lanskoder & str_length(alla_regionkoder) > 7]
-    region_kommun <- alla_regionkoder[str_sub(alla_regionkoder, 1,4) %in% lanskoder & str_length(alla_regionkoder) > 7]
+    region_kommun <- alla_regionkoder[str_sub(alla_regionkoder, 1,4) %in% kommunkoder & str_length(alla_regionkoder) > 7]
     
     # vi lägger ihop vektorerna för färdiga RegSO/DeSO, samt RegSO/DeSO för de län och kommuner som skickats med samt tar bort dubletter
     alla_region <- c(fardiga_regionkoder, region_lan, region_kommun) %>% .[!duplicated(.)]
     
   } else alla_region <- alla_regionkoder                     # om användaren vill ha samtliga koder för RegSO eller DeSO
+  
+  # Tidigare, felaktigt
+  # if (region_vekt != "*") {
+  #   # här delar vi upp de medskickade regionkoderna i RegSO/DeSO, länskoder och kommunkoder 
+  #   fardiga_regionkoder <- region_vekt[str_length(region_vekt) == 9]           # RegSO eller DeSO
+  #   lanskoder <- region_vekt[str_length(region_vekt) == 2]                     # länskoder
+  #   kommunkoder <- region_vekt[str_length(region_vekt) == 4]                   # kommunkoder
+  #   
+  #   # här hämtar vi alla RegSO/DeSO för de länskoder eller kommunkoder som skickats med
+  #   region_lan <- alla_regionkoder[str_sub(alla_regionkoder, 1,2) %in% lanskoder & str_length(alla_regionkoder) > 7]
+  #   region_kommun <- alla_regionkoder[str_sub(alla_regionkoder, 1,4) %in% lanskoder & str_length(alla_regionkoder) > 7]
+  #   
+  #   # vi lägger ihop vektorerna för färdiga RegSO/DeSO, samt RegSO/DeSO för de län och kommuner som skickats med samt tar bort dubletter
+  #   alla_region <- c(fardiga_regionkoder, region_lan, region_kommun) %>% .[!duplicated(.)]
+  #   
+  # } else alla_region <- alla_regionkoder                     # om användaren vill ha samtliga koder för RegSO eller DeSO
   #########################################################################################################################
   
   # Gör om från klartext
@@ -93,9 +111,9 @@ hamta_data_valdeltagande_regso <- function(region_vekt = "20",
   
   # man kan välja bort long-format, då låter vi kolumnerna vara wide om det finns fler innehållsvariabler, annars
   # pivoterar vi om till long-format, dock ej om det bara finns en innehållsvariabel
-  if (long_format & length(cont_klartext) > 1) {
+  if (long_format & !wide_om_en_contvar) {
     valdeltagande <- valdeltagande %>% 
-      konvertera_till_long_for_contentscode_variabler(api_url = px_meta, content_var = "ohalso_variabel")
+      konvertera_till_long_for_contentscode_variabler(api_url = px_meta, content_var = "valdeltagande_variabel")
   } # slut if-sats som kontrollera om vi vill ha df i long-format
   
   if (!is.na(output_mapp) & !is.na(filnamn)){
