9395673d4769cc2dba91abca3561d1d6a5819df1
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2026-07-13 08:17:35 +0200

    Uppdaterat felaktig API-adress

--- hamta_konkurser_manad_lan_variabel_konk_1996_tva.R
+++ hamta_konkurser_manad_lan_variabel_konk_1996_tva.R
@@ -21,6 +21,8 @@ hamta_konkurser_manad_lan_variabel_tva <- function(
   # Nygammal: https://statistik.tillvaxtanalys.se:443/PxWeb/api/v1/sv/Tillväxtanalys statistikdatabas/Konkurser och offentliga ackord/konk_manad_lan_1996.px
   # Byte igen: https://statistik.tillvaxtanalys.se:443/PxWeb/api/v1/sv/Tillväxtanalys statistikdatabas/Konkurser och offentliga ackord/konkurser_manad_lan.px
   # Har dessutom ändrat vissa av namnen i varlista
+  #
+  # Ändrat api-adress igen. Jon 2026-07-13
   # ====================================================================================================
   
   if (!require("pacman")) install.packages("pacman")
@@ -32,7 +34,7 @@ hamta_konkurser_manad_lan_variabel_tva <- function(
   source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R")
   
   # Url till SCB:s databas
-  url_uttag <- "https://statistik.tillvaxtanalys.se:443/PxWeb/api/v1/sv/Tillväxtanalys statistikdatabas/Konkurser och offentliga ackord/konkurser_manad_lan.px"
+  url_uttag <- "https://statistik.tillvaxtanalys.se:443/PxWeb/api/v1/sv/Tillväxtanalys statistikdatabas/Konkurser och offentliga ackord/11_konkurser_manad_lan.px"
   px_meta <- pxweb_get(url_uttag)
   
   varlist_koder <- pxvarlist(px_meta)$koder
